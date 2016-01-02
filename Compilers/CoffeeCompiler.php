shnhrrsn/laravel-assets

commit 6e5fc29c822bf54250a38397426d38887c66390d

Split up CoffeeCompiler to be more native and drop bash scripting

Compilers/CoffeeCompiler.php
@@ -1,17 +1,40 @@
 <?php namespace Assets\Compilers;
 
+use BadMethodCallException;
+
 use Symfony\Component\Process\Process;
+use Assets\Exceptions\CompilationException;
 
 class CoffeeCompiler extends ProcessCompiler {
 
+	public function compile($path, $context = null) {
+		$tmp = tempnam(sys_get_temp_dir(), sha1($path)) . '.js';
+		touch($tmp); // Ensures file always exists for unlinking
+
+		try {
+			$this->compileProcess(new Process('importer ' . escapeshellarg($path) . ' ' . escapeshellarg($tmp)));
+		} catch(CompilationException $e) {
+			unlink($tmp);
+			throw $e;
+		}
+
+		if(!$this->autoMinify) {
+			$output = file_get_contents($tmp);
+			unlink($tmp);
+			return $output;
+		} else {
+			try {
+				return $this->compileProcess(new Process('uglifyjs --compress drop_console=true ' . escapeshellarg($tmp)));
+			} catch(CompilationException $e) {
+				throw $e;
+			} finally {
+				unlink($tmp);
+			}
+		}
+	}
+
 	protected function getCompileProcess($path, $context = null) {
-		$out = escapeshellarg(tempnam(sys_get_temp_dir(), sha1($path)));
-		$uglify = $this->autoMinify ? ' | uglifyjs --compress drop_console=true' : '';
-		return new Process(
-			'bash -c "importer ' . escapeshellarg($path) . ' ' . $out .
-				' && cat ' . $out . $uglify .
-				' && rm ' . $out . '"'
-		);
+		throw new BadMethodCallException();
 	}
 
 	public function getLastModified($file, $newest = 0) {

Compilers/ProcessCompiler.php
@@ -22,8 +22,10 @@ abstract class ProcessCompiler extends Compiler {
 	protected abstract function getCompileProcess($path, $context = null);
 
 	public function compile($path, $context = null) {
-		$process = $this->getCompileProcess($path, $context);
+		return $this->compileProcess($this->getCompileProcess($path, $context));
+	}
 
+	protected function compileProcess(Process $process) {
 		$process->setEnv([
 			'PATH' => trim(`echo \$PATH`) . ':' . implode(':', $this->paths)
 		]);

commit e301b408f98546fc6d5bfbbe72c16fb59f0ec439

Quick fix for minor bug introduced in 6e5fc29c822bf54250a38397426d38887c66390d

Compilers/CoffeeCompiler.php
@@ -12,7 +12,7 @@ class CoffeeCompiler extends ProcessCompiler {
 		touch($tmp); // Ensures file always exists for unlinking
 
 		try {
-			$this->compileProcess(new Process('importer ' . escapeshellarg($path) . ' ' . escapeshellarg($tmp)));
+			$this->compileProcess(new Process('importer ' . escapeshellarg($path) . ' ' . escapeshellarg($tmp)), $path);
 		} catch(CompilationException $e) {
 			unlink($tmp);
 			throw $e;
@@ -24,7 +24,7 @@ class CoffeeCompiler extends ProcessCompiler {
 			return $output;
 		} else {
 			try {
-				return $this->compileProcess(new Process('uglifyjs --compress drop_console=true ' . escapeshellarg($tmp)));
+				return $this->compileProcess(new Process('uglifyjs --compress drop_console=true ' . escapeshellarg($tmp)), $path);
 			} catch(CompilationException $e) {
 				throw $e;
 			} finally {

Compilers/ProcessCompiler.php
@@ -22,10 +22,10 @@ abstract class ProcessCompiler extends Compiler {
 	protected abstract function getCompileProcess($path, $context = null);
 
 	public function compile($path, $context = null) {
-		return $this->compileProcess($this->getCompileProcess($path, $context));
+		return $this->compileProcess($this->getCompileProcess($path, $context), $path);
 	}
 
-	protected function compileProcess(Process $process) {
+	protected function compileProcess(Process $process, $path) {
 		$process->setEnv([
 			'PATH' => trim(`echo \$PATH`) . ':' . implode(':', $this->paths)
 		]);