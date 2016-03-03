dd412b44a01d7c351edea95ca3acae87854aa1e1
shnhrrsn <shnhrrsn@gmail.com>
2016-03-03 14:05:33 -0500

    Broke up UnpublishCommand to be more extendable

--- Console/UnpublishCommand.php
+++ Console/UnpublishCommand.php
@@ -7,15 +7,25 @@ class UnpublishCommand extends BaseCommand {
 	public $description = 'Removes published assets';
 
 	public function fire() {
+		$this->removePublishedAssets();
+		$this->removeConfig();
+	}
+
+	protected function removePublishedAssets() {
 		chdir(public_path());
 
 		$dirs = [ ];
+
 		foreach(config('published_assets') as $asset) {
 			$this->info('Removing asset: ' . $asset);
 			$dirs[] = realpath(dirname($asset));
 			$this->removeAsset($asset);
 		}
 
+		$this->removeEmptyDirectories($dirs);
+	}
+
+	protected function removeEmptyDirectories($dirs) {
 		foreach($dirs as $dir) {
 			if(empty($dir) || !file_exists($dir)) continue;
 
@@ -24,8 +34,10 @@ class UnpublishCommand extends BaseCommand {
 				@rmdir($dir);
 			}
 		}
+	}
 
-		@unlink(base_path() . "/config/published_assets.php");
+	protected function removeConfig() {
+		@unlink(base_path() . '/config/published_assets.php');
 	}
 
 }
